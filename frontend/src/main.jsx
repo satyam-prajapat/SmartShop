49736148d5920c477c1dcf4e58ddf3eff6085142
satyam <satyasatyam722@gmail.com>
2026-01-11 18:19:39 +0530

    fix: file casing for vercel deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/authContext.jsx'
-import UserContext from './context/UserContext.jsx'
-import ShopContext from './context/ShopContext.jsx'
+import AuthContext from './Context/AuthContext.jsx'
+import UserContext from './Context/UserContext.jsx'
+import ShopContext from './Context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
